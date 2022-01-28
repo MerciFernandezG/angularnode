832b55cbd0e398b954761f8f131c3fde766209af
ramoeg04 <4494.geo@gmail.com>
2022-01-28 15:13:57 -0400

    Estructura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './components/list/list/list.component';
 import { RegisterComponent } from './components/register/register/register.component';
-import { NavbarComponent } from './components/navbar/navbar.component'
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ViewComponent } from './components/view/view.component';
+import { HomeComponent } from './components/home/home.component'
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
     RegisterComponent,
-    NavbarComponent
+    NavbarComponent,
+    ViewComponent,
+    HomeComponent
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [], 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
